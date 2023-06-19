fcea53e7296834703398471783dd479a91875b2b
cluedon <134339245+cluedon@users.noreply.github.com>
2023-06-19 22:10:23 +0100

    react.js
    install ReactJS

---
+++ src/react.js
@@ -0,0 +1,22 @@
+{
+  "name": "decentralized-identity-app",
+  "version": "1.0.0",
+  "dependencies": {
+    "react": "^17.0.2",
+    "react-dom": "^17.0.2"
+  }
+}
+// App.js
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Decentralized Identity App</h1>
+      {/* Add your component code here */}
+    </div>
+  );
+}
+
+export default App;
