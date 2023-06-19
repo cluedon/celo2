6efc959ab46e8c67e4b50712c2e477d51acd14a7
cluedon <134339245+cluedon@users.noreply.github.com>
2023-06-19 09:28:54 +0100

    App.js
    This will start the development server, and you can access the app in your web browser at http://localhost:3000. The app will connect to the Celo network and retrieve the identity from the deployed smart contract.
    
    Please note that this is a simplified example and assumes that you have already deployed a Solidity smart contract for managing identities on the Celo Blockchain. You may need to modify and expand this code to meet your specific requirements and contract functionality.

---
+++ front end/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import IdentityApp from './IdentityApp';
+
+function App() {
+  return (
+    <div className="App">
+      <IdentityApp />
+    </div>
+  );
+}
+
+export default App;
